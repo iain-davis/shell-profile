09c876cdf13b15e1cf12b9509dd7345c704d4f3b
Iain E. Davis <iain@iaindavis.name>
2019-08-31 13:54:14 -0600

    Set upstream when creating branch

--- git-with-gradle/createBranch.sh
+++ git-with-gradle/createBranch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-set -x;                      # Echo commands to the screen
-git checkout master       && # Checkout master branch
-git pull origin master    && # Pull latest commits for the master branch
-./gradlew clean check     && # Make sure master branch's tests are passing
-git checkout -b $1        && # Create new branch
-git push origin $1;          #
-set +x                       # Stop echoing commands
+set -x;                            # Echo commands to the screen
+git checkout master             && # Checkout master branch
+git pull origin master          && # Pull latest commits for the master branch
+./gradlew clean check           && # Make sure master branch's tests are passing
+git checkout -b $1              && # Create new branch
+git push --set-upstream origin $1; #
+set +x                             # Stop echoing commands
