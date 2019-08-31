e8316bcbdbb747464dc858c87185509ce577d7b7
Iain E. Davis <iain@iaindavis.name>
2019-08-31 11:38:13 -0600

    Add createBranch script

--- git-with-gradle/shell-profile-git-with-gradle.sh
+++ git-with-gradle/shell-profile-git-with-gradle.sh
@@ -2,8 +2,8 @@
 # This script assumes that scriptDirectory has already been set and depends on shell-profile-git.sh
 
 # Aliased scripts that take a branch as an argument
+alias createBranch='$scriptDirectory/createBranch.sh'
 alias removeBranch='$scriptDirectory/removeBranch.sh'
-
 
 # Setup tab completion for the aliases
 complete -F _git_branch_complete removeBranch
