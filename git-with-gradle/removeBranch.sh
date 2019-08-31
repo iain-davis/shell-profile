e294a3508a4ba0a10889822d65de88a9ee7b9b0f
Iain E. Davis <iain@iaindavis.name>
2019-08-31 14:03:55 -0600

    Add some more comments

--- git-with-gradle/removeBranch.sh
+++ git-with-gradle/removeBranch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -x;                      # Echo commands to the screen
-git branch -D $1
-git push origin :$1;
+git branch -D $1             # Delete local replica of the branch
+git push origin :$1;         # Delete remote replica of the branch
 set +x                       # Stop echoing commands
