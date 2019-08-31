5fadc27e54236f1570af4131384e172846b8b97d
Iain E. Davis <iain@iaindavis.name>
2019-08-31 12:54:03 -0600

    Make secondary scripts actually work

--- shell-profile.sh
+++ shell-profile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo Configuring shell...
-scriptDirectory="$(dirname "${BASH_SOURCE[0]}")"
+scriptDirectory=$(dirname "$(realpath -s "$BASH_SOURCE[0]")") # Get the directory in which the main shell script resides. With thanks to Darshan Rivka Whittle @ https://stackoverflow.com/questions/4774054/reliable-way-for-a-bash-script-to-get-the-full-path-to-itself
+echo Configuring shell using scripts in $scriptDirectory
 
 # Git related configuration
 source "$scriptDirectory/git/shell-profile-git.sh"
