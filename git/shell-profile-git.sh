4e37c7ff47ad929790e88b190dc7afd87fd3c0aa
Iain E. Davis <iain@iaindavis.name>
2019-09-11 20:45:46 -0600

    Add gcc alias

--- git/shell-profile-git.sh
+++ git/shell-profile-git.sh
@@ -17,6 +17,7 @@ alias swap='git checkout -'
 alias gc='git checkout'
 alias push='git push origin'
 alias pull='git pull origin'
+alias gcc='./gradlew clean check'
 
 complete -F _git_branch_complete gc
 complete -F _git_branch_complete push
