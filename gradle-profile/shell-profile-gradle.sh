47bf8545f583250977633543df538257fec962f1
Iain Davis <IainDavis@carfax.com>
2022-03-23 16:09:55 -0500

    Add 'gw' alias

--- gradle-profile/shell-profile-gradle.sh
+++ gradle-profile/shell-profile-gradle.sh
@@ -1,5 +1,6 @@
 echo Setting up gradle related commands...
 export GRADLE_OPTS=-Dorg.gradle.daemon=false
+alias gw='./gradlew'
 alias gcc='./gradlew clean check'
 alias gccr='./gradlew clean check jettyRun'
 alias gccrw='./gradlew clean check jettyRunWar'
