1a55b8668514bada23bb6b88b4f6ecc7e5521f33
Iain E. Davis <iaindavis@carfax.com>
2022-06-06 13:11:07 -0500

    Rename some colors

--- color.sh
+++ color.sh
@@ -13,11 +13,11 @@ Cyan='\033[0;36m'         # Cyan
 White='\033[0;37m'        # White
 
 # Bold
-BBlack='\033[1;30m'       # Black
+Grey='\033[1;30m'         # Bright Black = Grey
 BRed='\033[1;31m'         # Red
 BGreen='\033[1;32m'       # Green
 BYellow='\033[1;33m'      # Yellow
-BBlue='\033[1;34m'        # Blue
+BrightBlue='\033[1;34m'    # Bright Blue
 BPurple='\033[1;35m'      # Purple
 BCyan='\033[1;36m'        # Cyan
 BWhite='\033[1;37m'       # White
