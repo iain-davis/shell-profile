27d35bfb9aebdd8a27fdbd5533badc34f9be5f97
Iain E. Davis <iain@iaindavis.name>
2022-06-17 13:46:52 -0600

    Additional aliases

--- gradle-profile/shell-profile-gradle.sh
+++ gradle-profile/shell-profile-gradle.sh
@@ -1,4 +1,5 @@
 echo Setting up gradle related commands...
 alias gcc='./gradlew clean check'
-alias gccr='./gradlew clean check jettyRun'
-alias gccrw='./gradlew clean check jettyRunWar'
+alias gccjr='./gradlew clean check jettyRun'
+alias gccjrw='./gradlew clean check jettyRunWar'
+alias gccr='./gradlew clean check run'
