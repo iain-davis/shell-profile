b42ef9929fe734a88e5fc4109265010a7986dcc0
Iain Davis <iaindavis@carfax.com>
2025-08-05 10:16:57 -0500

    Fix issue where the wrong realpath is used on MacOS. Also fix shebang

--- shell-profile.sh
+++ shell-profile.sh
@@ -1,7 +1,17 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+# With thanks to: https://stackoverflow.com/questions/394230/how-to-detect-the-os-from-a-bash-script
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    # Prerequisite for dirname and realpath to work correctly below
+    # Will also require coreutils to have been installed via brew
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+    export PATH="/opt/homebrew/opt/coreutils/libexec/gnubin:/opt/homebrew/bin:$PATH"
+fi
+
 # Get the directory in which the main shell script resides. With thanks to Darshan Rivka Whittle @ https://stackoverflow.com/questions/4774054/reliable-way-for-a-bash-script-to-get-the-full-path-to-itself
 # For this to work on macOS, 'realpath' needs to be installed: brew install coreutils
 scriptDirectory=$(dirname "$(realpath -s "${BASH_SOURCE[0]}")")
+
 source "$scriptDirectory/shell-profile-color.sh"
 source "$scriptDirectory/shell-profile-color-prompt.sh"
 echo -e Configuring shell using scripts in ${Green}"$scriptDirectory"${Reset}...
